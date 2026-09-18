21751a7b0d54e725d8270e766ea04cc6ea9e29e8
Arya Patil <work.aryapatil3455@gmail.com>
2026-09-18 17:20:04 +0530

    Initial project setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT Support Portal</title>
+</head>
+
+<body>
+
+    <h1>IT Support Portal</h1>
+    <p>working.</p>
+
+</body>
+</html>
