130f04b0cf061937acea3915f82227f33d728cff
Arya Patil <work.aryapatil3455@gmail.com>
2026-09-18 18:27:44 +0530

    Add database configuration

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "itsupport";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Database connection failed.");
+}
+
+$conn->set_charset("utf8mb4");
