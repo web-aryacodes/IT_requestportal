7c5f440cab2ed226f99ee0f3772bd6625fd7aace
Arya Patil <work.aryapatil3455@gmail.com>
2026-09-23 17:39:26 +0530

    Fix employee logout security

--- employee/ticket.php
+++ employee/ticket.php
@@ -54,9 +54,16 @@ if (!$ticket) {
         <p>Internal IT Helpdesk System</p>
     </div>
 
-    <button onclick="window.location.href='../auth/logout.php'">
-        Logout
-    </button>
+        <form method="POST" action="../auth/logout.php">
+            <input
+                type="hidden"
+                name="csrf_token"
+                value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>"
+            >
+            <button type="submit" class="action-btn secondary">
+                Logout
+            </button>
+        </form>
 </div>
 
 <div class="container">
